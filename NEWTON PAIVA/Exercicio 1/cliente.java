904f6f39b1f61147d5f9e728b4e6227c9824741d
becona <109755359+BecaFrancine24@users.noreply.github.com>
2022-09-02 14:20:10 -0300

    Exercicio de POO

---
+++ NEWTON PAIVA/Exercicio 1/cliente.java
@@ -0,0 +1,13 @@
+package Exercicio1;
+
+public class cliente {
+
+	private String nome;
+	private String CPF;
+	private String RG;
+	private String CNPJ;
+	private boolean tipoCliente;
+	private String telefone;
+	private String email;
+	
+}
