fd04c24d426bd5e2e6060375936cc732d1ba7880
BecaFrancine24 <109755359+BecaFrancine24@users.noreply.github.com>
2022-09-15 20:07:56 -0300

    Add files via upload

---
+++ PRATICA 2/BaseRendimento.java
@@ -0,0 +1,23 @@
+package br.com.Rendimento;
+
+import java.util.Scanner;
+
+public class BaseRendimento {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		System.out.println("Digite o valor inicial do Rendimento: ");
+		double vrInicial = ler.nextDouble();
+
+		System.out.println("Digite a taxa de juros: ");
+		double txJuros = ler.nextDouble();
+		
+		System.out.println("Digite o numero de meses para o Rendimento: ");
+		int numMeses = ler.nextInt();
+		
+		Rendimento r1 = new Rendimento(vrInicial,txJuros,numMeses);
+		r1.calculaRendimento();
+		
+	}
+
+}
