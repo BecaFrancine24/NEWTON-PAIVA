3c96f0dc2e3c2db6d699254b83aee1d9195167dc
becona <109755359+BecaFrancine24@users.noreply.github.com>
2022-10-19 13:29:30 -0300

    ABACAXI

---
+++ Exercicio_Fixacao/exercicioFixacao/src/exercicioFixacao/pizza.java
@@ -0,0 +1,36 @@
+package exercicioFixacao;
+
+public class pizza {
+
+	private String nome;
+	private Float valor;
+	private Float tamanho;
+	private Boolean possuiBordaRecheada;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Float getValor() {
+		return valor;
+	}
+	public void setValor(Float valor) {
+		this.valor = valor;
+	}
+	public Float getTamanho() {
+		return tamanho;
+	}
+	public void setTamanho(Float tamanho) {
+		this.tamanho = tamanho;
+	}
+	public Boolean getPossuiBordaRecheada() {
+		return possuiBordaRecheada;
+	}
+	public void setPossuiBordaRecheada(Boolean possuiBordaRecheada) {
+		this.possuiBordaRecheada = possuiBordaRecheada;
+	}
+	
+	
+
+}
